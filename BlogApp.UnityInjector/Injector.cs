a2c759953bd3e04398ecf8b4c748368aa92fae86
Pablo <pabloperfalc@gmail.com>
2015-08-01 15:25:18 -0300

    Agregados post build event

--- BlogApp.UnityInjector/Injector.cs
+++ BlogApp.UnityInjector/Injector.cs
@@ -1,4 +1,7 @@
 ﻿using BlogApp.Manager.Implementations;
+using BlogApp.UnityInjector;
+using BlogApp.Web;
+using BlogApp.Web.Controllers;
 using BlogApp.Web.RequiredInterfaces;
 using Microsoft.Practices.Unity;
 using System;
@@ -7,15 +10,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//Copiar dll UnityINjetor al bin del WEB
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(Injector), "Inject")]
+
 namespace BlogApp.UnityInjector
 {
     public class Injector
     {
-        static Injector()
+        public static void Inject()
         {
             var container = new UnityContainer();
 
             container.RegisterType<IUserManager, UserManager>();
+
+            InjectorResolver.SetResolver(container);
         }
     }
 }
---
+++ BlogApp.Web/InjectorResolver.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogApp.Web
+{
+    public class InjectorResolver
+    {
+        public static void SetResolver(UnityContainer container)
+        {
+            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+        }
+    }
+}
