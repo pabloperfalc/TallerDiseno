4f7e505fc76ad03303d25265a555f471e38759f9
Alejandro Alvarez <alealvarez14@gmail.com>
2015-08-08 22:17:32 -0300

    Enum logtype

---
+++ BlogApp.ILogger/LogType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlogApp.ILogger
+{
+    enum LogType
+    {
+        Login,
+        Search
+    }
+}
