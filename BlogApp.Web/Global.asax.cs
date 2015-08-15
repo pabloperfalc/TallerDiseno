dc85c9237d95e6eed3414c5fcb5e94685e8ef901
Pablo <pabloperfalc@gmail.com>
2015-08-15 18:25:36 -0300

    layout and bootstrap

--- BlogApp.Web/Global.asax.cs
+++ BlogApp.Web/Global.asax.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
+using WebApplication1;
 
 namespace BlogApp.Web
 {
@@ -19,6 +21,7 @@ namespace BlogApp.Web
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
