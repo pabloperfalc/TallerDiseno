abc38fef69de132ffe90ffda23dac7d85e90ac00
Pablo <pabloperfalc@gmail.com>
2015-08-23 13:28:46 -0300

    Fix

--- BlogApp.Web/RequiredInterfaces/IArticleManager.cs
+++ BlogApp.Web/RequiredInterfaces/IArticleManager.cs
@@ -10,14 +10,10 @@ namespace BlogApp.Web.RequiredInterfaces
     public interface IArticleManager
     {
         List<Article> GetLatest(int count);
-<<<<<<< HEAD
         List<Article> Cargar(XmlDocument xml);
-=======
         List<Tuple<string, string>> ValidateArticle(Article article);
         void AddArticle(Article article);
         void UpdateArticle(Article article);
         void DeleteArticle(Article article);
-
->>>>>>> 74bd980db486fc924ec24ae1d7b137715d06dd99
     }
 }
