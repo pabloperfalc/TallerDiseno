b29990ca6cefc9af2821aaf37da551cc89bec7fc
Diego Rocca <diegorocca15@gmail.com>
2015-08-23 13:09:44 -0300

    Commit

--- BlogApp.Manager/Implementations/ArticleManager.cs
+++ BlogApp.Manager/Implementations/ArticleManager.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml;
 
 namespace BlogApp.Manager.Implementations
 {
@@ -21,5 +22,25 @@ namespace BlogApp.Manager.Implementations
         {
             return articleDataAccess.GetLatest(count);
         }
+
+        public List<Article> Cargar(XmlDocument xml)
+        {
+            List<Article> listaArticulos = new List<Article>();
+            XmlNodeList articulos = xml.SelectNodes("//Articulos/Articulo");
+            foreach (XmlNode nodo in articulos)
+            {
+                Article art = new Article 
+                                    {
+                                        Name = nodo.SelectSingleNode("Nombre").InnerText,
+                                        Text = nodo.SelectSingleNode("Texto").InnerText,
+                                        //Type = nodo.SelectSingleNode("Tipo").InnerText,
+                                        PicturePath = nodo.SelectSingleNode("Foto").InnerText,
+                                        //Layout = nodo.SelectSingleNode("Plantilla").InnerText
+                                    };
+                listaArticulos.Add(art);
+            }
+            return listaArticulos;
+        }
+        
     }
 }
--- BlogApp.Web/RequiredInterfaces/IArticleManager.cs
+++ BlogApp.Web/RequiredInterfaces/IArticleManager.cs
@@ -3,11 +3,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml;
 
 namespace BlogApp.Web.RequiredInterfaces
 {
     public interface IArticleManager
     {
         List<Article> GetLatest(int count);
+        List<Article> Cargar(XmlDocument xml);
     }
 }
