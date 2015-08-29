83464ab2854677659a87c1f40f622821a542f849
Pablo <pabloperfalc@gmail.com>
2015-08-29 17:40:05 -0300

    video fail

--- BlogApp.VideoPlayer/MainPage.xaml.cs
+++ BlogApp.VideoPlayer/MainPage.xaml.cs
@@ -21,16 +21,56 @@ namespace BlogApp.VideoPlayer
         {
             InitializeComponent();
 
-            Uri serviceUri = new Uri("/video/getVideoPaths", UriKind.Relative);
-            WebClient downloader = new WebClient();
-            
-            downloader.OpenReadCompleted += downloader_OpenReadCompleted;
-            downloader.OpenReadAsync(serviceUri);
+            GetVideos();
+        }
+
+        private void GetVideos()
+        {
+            try
+            {
+                Uri serviceUri = new Uri("/video/getVideoPaths", UriKind.Relative);
+                WebClient downloader = new WebClient();
+
+                downloader.OpenReadCompleted += downloader_OpenReadCompleted;
+                downloader.OpenReadAsync(serviceUri);
+            }
+            catch
+            {
+                System.Threading.Thread.Sleep(15000);
+                GetVideos();
+            }
+
         }
 
         private void player_MediaEnded(object sender, RoutedEventArgs e)
         {
             player.Stop();
+            PlayNextVideo();
+        }
+
+        void downloader_OpenReadCompleted(object sender, OpenReadCompletedEventArgs e)
+        {
+            var jsonSerializer = new DataContractJsonSerializer(typeof(List<string>));
+            videoList = (List<string>)jsonSerializer.ReadObject(e.Result);
+            if (videoList != null && videoList.Count > 0)
+            {
+                player.Source = new Uri(videoList[0]);
+                player.Play();
+            }
+            else
+            {
+                System.Threading.Thread.Sleep(15000);
+                GetVideos();
+            }
+        }
+
+        private void player_MediaFailed(object sender, ExceptionRoutedEventArgs e)
+        {
+            PlayNextVideo();
+        }
+
+        private void PlayNextVideo()
+        {
             index++;
             if (index >= videoList.Count)
             {
@@ -38,19 +78,6 @@ namespace BlogApp.VideoPlayer
             }
             player.Source = new Uri(videoList[index]);
             player.Play();
-        }
-
-        void downloader_OpenReadCompleted(object sender, OpenReadCompletedEventArgs e)
-        {
-            var jsonSerializer = new DataContractJsonSerializer(typeof(List<string>));
-            videoList = (List<string>)jsonSerializer.ReadObject(e.Result);
-            player.Source = new Uri(videoList[0]);
-            player.Play();
-        }
-
-        private void player_MediaFailed(object sender, ExceptionRoutedEventArgs e)
-        {
-
         }
     }
 }
